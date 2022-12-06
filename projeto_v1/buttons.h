6579be395ea9ab847c0afba1ed103c8323f89899
Guilherme Moreira <93450195+gm-28@users.noreply.github.com>
2022-12-06 19:15:47 +0000

    Delete projeto_v1 directory

--- projeto_v1/buttons.h
+++
@@ -1,9 +0,0 @@
-#ifndef BUTTONS_H
-#define BUTTONS_H
-
-#include <Arduino.h>
-
-void set_up_button(byte pin);
-int read_buttons(byte pin);
-
-#endif
--- projeto_v1/displays.h
+++
@@ -1,9 +0,0 @@
-#ifndef DISPLAYS_H
-#define DISPLAYS_H
-
-#include "Adafruit_LiquidCrystal.h"
-#include <Arduino.h>
-
-void lcd_refresh(Adafruit_LiquidCrystal *l, int *game_rows);
-
-#endif
