a34a80f9f7de992d41c3e9f48fcd55252e5215f6
Guilherme Moreira <93450195+gm-28@users.noreply.github.com>
2022-12-07 00:44:19 +0000

    Add files via upload

--- projeto/projeto_v1.ino
+++ projeto/projeto_v1.ino
@@ -15,8 +15,13 @@
 Adafruit_LiquidCrystal lcd1(0x20); // DAT -> A4 CLK -> A5
 Adafruit_LiquidCrystal lcd2(0x21); // DAT -> A4 CLK -> A5
 
-int buttonState_1 = 0;  // variable for reading the pushbutton status
-int buttonState_2 = 0;  // variable for reading the pushbutton status
+int button1State;            // the current reading from the input pin
+int lastButton1State = LOW;  // the previous reading from the input pin
+
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+int score = 0;
 bool flag = false;
 
 byte customChar[8] = {0b11111,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111}; // display character
@@ -34,43 +39,69 @@ ISR(TIMER1_COMPA_vect){
 void t1(void) {
   //Serial.print(" A");
   lcd_refresh(&lcd1, game_row(game_rows_1,rows_aux_1));
+  lcd_refresh(&lcd2, game_row(game_rows_2,rows_aux_2));
 } 
 
 void t2(void) {
   //Serial.print(" B");
-  lcd_refresh(&lcd2, game_row(game_rows_2,rows_aux_2));
+  //lcd_refresh(&lcd2, game_row(game_rows_2,rows_aux_2));
+  if(flag){
+    score +=1;
+    flag = false;
+    Serial.println(score);
+  }
 }
 
 void t3(void) {
-  buttonState_1 = read_buttons(B11); // read the state of the pushbutton value
+  button1State = read_buttons(B11); // read the state of the pushbutton value
   //buttonState_2 = read_buttons(B12); // read the state of the pushbutton value
   
-  if(buttonState_1==1 && game_rows_1[15]==2){
-    flag = true;
+  if (button1State == HIGH){
+    if(game_rows_1[15]==2){
+      flag=true;
+    }
   }
 }
 
-void t5(void) {
-  if(flag){
-    game_score();
-    flag = false;
-  }
-}
+/*
+void t4(void){
+  // read the state of the switch into a local variable:
+  int reading = read_buttons(B11);
 
-/*void t4(void) {
-  Serial.print("\nE");
-  buttonState_1 = read_buttons(B21); // read the state of the pushbutton value
-  buttonState_2 = read_buttons(B22); // read the state of the pushbutton value
-  
-  if(buttonState_1==1){
-    //score=score+game_button(game_rows[15]);
-    Serial.print(" F");
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButton1State) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != button1State) {
+      button1State = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (button1State == HIGH){
+          if(game_rows_1[15]==2){
+            score += 1;
+            Serial.println(score);
+            flag=false;
+          }
+        //flag = true;
+        //Serial.println(flag);
+      }
+    }
   }
   
-  Serial.print("\n");
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastButton1State = reading;
 }*/
-
-
 
 void setup() {
   //INIT SCHEDULER
@@ -78,8 +109,10 @@ void setup() {
   Sched_Init();
   Sched_AddT(t1, 0, 500);
   Sched_AddT(t2, 0, 500);
-  Sched_AddT(t3, 0, 5);
-  Sched_AddT(t5, 0, 500);
+  Sched_AddT(t3, 0, 50);
+  //Sched_AddT(t5, 0, 500);
+  //ched_AddT(t4, 0, 50);
+
   
   noInterrupts(); // disable all interrupts
   TCCR1A = 0;
