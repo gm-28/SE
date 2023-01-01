f9de232349acf4a5279555a9187dbf594869534d
Armazens <94800661+Armazens@users.noreply.github.com>
2023-01-01 16:33:50 +0000

    Update game_logic.cpp

--- projeto/game_logic.cpp
+++ projeto/game_logic.cpp
@@ -17,12 +17,12 @@ int game_state(){ // returns a random position 1 or 2 based on a random number g
   return n_row;
 }
 
-int *game_row(int *game_rows, int *rows_aux){ // Updates current game state
-  for(int i=15;i>=0;i--){                     // Copies the previous game state
+int *game_row(int *game_rows, int *rows_aux){ // updates current game state
+  for(int i=15;i>=0;i--){                     // copies the previous game state
     rows_aux[i]=game_rows[i];
   }
   game_rows[0]=game_state();
-  for(int i=15;i>=0;i--){                     // Updates array to match what should be the new game state
+  for(int i=15;i>=0;i--){                     // updates array to match what should be the new game state
     game_rows[i+1]=rows_aux[i];
   }
   return game_rows;
